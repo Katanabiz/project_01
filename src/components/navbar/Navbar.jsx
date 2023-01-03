d8266c3497251e8f669dbac391c3960990f3c58c
Tokzhan Beisekenova <tokzhan@longevityintime.org>
2023-01-03 20:56:56 +0800

    today task finshed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './navbar.css';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
+
+// BEM Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="gpt1__navbar">
+      <div className="gpt1__navbar-links">
+        <div className="gpt1__navbar-links-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt1__navbar-links-container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt1">What is GPT1</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
